0d202ee8d679137b372f323b2c23a01b8f78605d
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-06 23:17:42 +0900

    recalculate average

--- baekjoon/1546.py
+++ baekjoon/1546.py
@@ -7,13 +7,13 @@ N = int(input())
 # 점수 리스트 입력
 arr = list(map(int, input().split()))
 
-# 최고 점수 M
+# 최고 점수 M 찾기
 M = max(arr)
 
-# 점수 조정 합계 계산
+# 새로운 점수 합계 계산
 sum_score = 0
 for i in arr:
     sum_score += i / M * 100
 
-# 평균 출력
+# 새로운 평균 출력
 print(sum_score / N)
