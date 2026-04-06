847bd1ae827af9e9dd9663bced8c982e60ce70cb
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-06 23:07:15 +0900

    repeat addition

--- baekjoon/10950.py
+++ baekjoon/10950.py
@@ -1,7 +1,11 @@
 # problem: 10950
 # tier: bronze
 
+# 테스트 케이스 개수 입력
 T = int(input())
+
+# T번만큼 반복 수행
 for _ in range(T):
+    # 두 정수 입력 및 합계 출력
     A, B = map(int, input().split())
     print(A + B)
