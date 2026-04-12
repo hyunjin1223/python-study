8d7c32e513a182458ff7da5d40ce5d90304649b3
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-12 21:25:48 +0900

    char at index

--- baekjoon/27866.py
+++ baekjoon/27866.py
@@ -1,11 +1,11 @@
 # problem: 27866
 # tier: bronze
 
-# 문자열 S 입력
+# 문자열 입력
 S = input()
 
-# 출력할 문자 위치 n 입력
+# 위치 n 입력
 n = int(input())
 
-# n번째 문자 출력 (1-indexed)
+# n번째 문자 출력 (인덱스 보정)
 print(S[n-1])
