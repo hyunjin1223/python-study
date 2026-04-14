e9e1a39d4c03f56e158a41b045ca9f7be04bdd6a
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-14 23:07:56 +0900

    find maximum value

--- baekjoon/2562.py
+++ baekjoon/2562.py
@@ -1,22 +1,23 @@
 # problem: 2562
 # tier: bronze
 
-# 입력 받을 9개의 수를 리스트에 저장
+# 9개의 자연수 입력받아 리스트에 저장
 arr = []
 for i in range(9):
     num = int(input())
     arr.append(num)
 
-# 최댓값과 인덱스 초기화
+# 최댓값과 해당 인덱스 초기화
 max_val = arr[0]
 max_idx = 0
 
-# 리스트를 순회하며 최댓값과 위치 찾기
+# 반복문을 통해 최댓값과 그 위치 탐색
 for i in range(9):
     if arr[i] > max_val:
         max_val = arr[i]
         max_idx = i
 
-# 최댓값과 위치(1-index) 출력
+# 최댓값 출력
 print(max_val)
+# 1번째부터 시작하는 위치(index + 1) 출력
 print(max_idx + 1)
