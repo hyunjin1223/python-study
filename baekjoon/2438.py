07b49df3e284d1a8cfb82b560da6368bf9d9a000
Hyunjin Baek <hyunjin3399@gmail.com>
2026-04-18 00:30:27 +0900

    star pattern 1

--- baekjoon/2438.py
+++ baekjoon/2438.py
@@ -1,7 +1,10 @@
 # problem: 2438
 # tier: bronze
 
-A = int(input())
+# 별의 개수(N) 입력
+N = int(input())
 
-for i in range(1, A + 1):
-    print(("*") * i)
+# 1부터 N까지 반복하며 별 출력
+for i in range(1, N + 1):
+    # 각 줄마다 i개의 별 출력
+    print("*" * i)
